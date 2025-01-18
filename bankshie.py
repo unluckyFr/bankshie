3f117c7c864ad1d26f62fe034cd1f1d4ffd32760
unluckyFr <duxyunlucky@gmail.com>
2025-01-19 00:07:19 +0100

    added auto generated password

--- bankshie.py
+++ bankshie.py
@@ -47,7 +47,7 @@ class BankAccount:
         self.password  = new_pass
         print(f'Account details updated, name: {self.name}, age: {self.age}, withdrawl limit: {self.withdrawl_limit}, password: {new_pass}')
 
-def pass_validate(word):
+def pass_validate(word):  #validation function for the user passsword
     count= 0
     special_ca_set = '!@#$%^&*()_+-=`~:;/?.>,<{}[]'
     if len(word) > 8:
@@ -66,12 +66,24 @@ def pass_validate(word):
         count += 1
     else:
         print('Password has to include at least one number.')
-    if count == 5:
+    if count == 4:
         return True
     else:
         return False
 
-
+def random_password():  #instead of making a password, the user has the choice to generate one, in order to allways be inside the criteria, it takes 5 numbers, 5 capital letters, 5 special characters and 10 random ones and has a length of 25
+    special_char = '!@#$%^&*()_+=-~`,./?<>[]{}|;:\'\"\\'
+    characters = [chr(i) for i in range(33, 127)]  #all printable ascii characters
+    nums = '1234567890'
+    capital = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    capital = (r.choices(capital, k=5))  #5 capital letters e.g QHJRB
+    num = (r.choices(nums, k=5))  #5 numbers
+    special = (r.choices(special_char, k=5))  #5 special characters e.b: 19854
+    password = (r.choices(characters, k=10))  #10 random printable ascii characters
+    password += num + special + capital  #combine all of them together
+    r.shuffle(password)  #shuffle them since when we combine them the list is in order (random characters, numbers, special character and then capital characters)
+    password = ''.join(password)  #make the password a string
+    return password
 
 
 
@@ -79,18 +91,19 @@ def main():
     accounts = {}
 
     while True:  # other things: offers/store; support for different currencies(stored in a mini account); shortcut for transfer(no need 4 id) etc
-        print(f'Welcome! Select one option please:\n'
+        print(f'\nWelcome! Select one option please:\n'
               f'1. Create an account\n'
               f'2. View account details\n'
               f'3. Edit account details\n'
               f'4. Deposit money into your account\n'
               f'5. Withdraw money from your account\n'
               f'6. Transfer money into another account\n'
-              f'7. Our Store\n'
-              f'7. Exit')
+              f'7. View all accounts (ADMINS) only\n'
+              f'8. Our Store\n'
+              f'10. Exit')
 
         try:
-            choice = int(input('Input a number 1-10'))
+            choice = int(input('\nInput a number 1-10'))
         except TypeError:
             print('Please input a whole number ')
 
@@ -101,17 +114,24 @@ def main():
                 name = input('Input your name:  ')
                 age = int(input('Input your age:  '))
                 while True:
-                    password = input('Password criteria:\n'
-                                     'Longer than 8 characters\n'
-                                     'Has at least one special character\n'
-                                     'Contains at least one capital letter\n'
-                                     'Contains at least one number\n'
-                                     'Input a password:  ')
-                    if pass_validate(password):
-                        print('Password accepted.')
+                    c = input('\n1. To make a password\n2. To automatically generate one\n... ')
+                    if c =='1':
+                        password = input('Password criteria:\n'
+                                         'Longer than 8 characters\n'
+                                         'Has at least one special character\n'
+                                         'Contains at least one capital letter\n'
+                                         'Contains at least one number\n'
+                                         'Input a password:  ')
+                        if pass_validate(password):
+                            print('Password accepted.')
+                            break
+                        else:
+                            print('make a valid password.')
+                    elif c == '2':
+                        password = random_password()
                         break
                     else:
-                        print('make a valid password.')
+                        print('Input a valid option')
                 accounts[acc_id] = BankAccount(age, name, password)
                 print(
                     f'Account {acc_id} succsesfully created, name: {accounts[acc_id].name}| age: {accounts[acc_id].age}')
@@ -121,11 +141,15 @@ def main():
             case 2:  # view account details
                 acc_id = input('Input your account id please!')
                 if acc_id in accounts:
-                    print(f'Account id: {acc_id}\n'
-                          f'Name: {accounts[acc_id].name}'
-                          f'Age: {accounts[acc_id].age}'
-                          f'Current Balance: {accounts[acc_id].balance}'
-                          f'Withdrawl Limit: {accounts[acc_id].withdrawl_limit}')
+                    user_password = input('\nPlease input your password: ')
+                    if user_password == accounts[acc_id].password:
+                        print(f'Account id: {acc_id}\n'
+                              f'Name: {accounts[acc_id].name}'
+                              f'Age: {accounts[acc_id].age}'
+                              f'Current Balance: {accounts[acc_id].balance}'
+                              f'Withdrawal Limit: {accounts[acc_id].withdrawl_limit}')
+                    else:
+                        print('Wrong password try again!')
                 else:
                     print('This account id does not exist, make sure to input the correct id or create an account now!')
 
@@ -138,7 +162,7 @@ def main():
                         while True:  # input and validate new name
                             new_name = str(input('What is you new name?'))
                             if any(n.isdigit() for n in new_name):
-                                print('Can have a name with digits')
+                                print('Can\'t have a name with digits')
                             else:
                                 break
 
@@ -191,7 +215,7 @@ def main():
                 else:
                     print('This account id does not exist, make sure to input the correct id or create an account now!')
 
-            case 6:  # transfer momey
+            case 6:  # transfer money
                 acc_id = input('Enter your account id: ')
                 other_id = input('Enter the id for hte target account:  ')
                 if acc_id in accounts:
